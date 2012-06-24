56819bd9ba51e6d858839de6522d29894174e297
JieeHD <jordan.craig94@gmail.com>
2012-06-24 10:36:44 +0100

    add getROMVersion to background task, hopefully fixes the UI lockups while the thread is executing

--- src/com/jieehd/villain/toolkit/utils/Utils.java
+++ src/com/jieehd/villain/toolkit/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.jieehd.villain.toolkit.utils;
 
+import android.os.AsyncTask;
 import android.os.Build;
 
 import com.jieehd.villain.toolkit.utils.ShellCommand.CommandResult;
@@ -7,21 +8,35 @@ import com.jieehd.villain.toolkit.utils.ShellCommand.CommandResult;
 public class Utils {
 	public static final String LOGTAG = "VillainToolkit";
 
-	public static String getROMVersion() {
-		ShellCommand cmd = new ShellCommand();
-		CommandResult modversion = cmd.su.runWaitFor("getprop ro.modversion");
-		if(modversion.stdout.equals("")) {
-			CommandResult cmversion = cmd.su.runWaitFor("getprop ro.cm.version");
-			if(cmversion.stdout.equals("")) {
-				CommandResult aokpversion = cmd.su.runWaitFor("getprop ro.aokp.version");
-				if(aokpversion.stdout.equals(""))
-					return Build.DISPLAY;
-				else
-					return aokpversion.stdout;
-			} else {
-				return cmversion.stdout;
+	
+	public class Read extends AsyncTask<String, Integer, String> {
+	
+		@Override
+		protected String doInBackground(String... params) {
+			
+			ShellCommand cmd = new ShellCommand();
+			
+			CommandResult modversion = cmd.su.runWaitFor("getprop ro.modversion");
+			
+			if(modversion.stdout.equals("")) {				
+				CommandResult cmversion = cmd.su.runWaitFor("getprop ro.cm.version");
+				
+				if(cmversion.stdout.equals("")) {
+					CommandResult aokpversion = cmd.su.runWaitFor("getprop ro.aokp.version");
+					
+					if(aokpversion.stdout.equals(""))						
+						return Build.DISPLAY;
+					
+					else					
+						return aokpversion.stdout;
+				
+				} else {					
+					return cmversion.stdout;
+				}
 			}
+			
+			return modversion.stdout;
 		}
-		return modversion.stdout;
+	
 	}
 }
